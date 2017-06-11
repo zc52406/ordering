701b30e9340c8e6ab90c9a1c2f6c08e4dd95bff9
wangyingjun <277933360@qq.com>
2017-06-11 14:42:48 +0800

    商品评价页面

---
+++ pages/goods/assessment.js
@@ -0,0 +1,26 @@
+// pages/goods/assessment.js
+Page({
+  data:{
+    currentLength: 0
+  },
+  inputHandler: function(e){
+    this.setData({
+      currentLength: e.detail.value.length
+    })
+  },
+  onLoad:function(options){
+    // 页面初始化 options为页面跳转所带来的参数
+  },
+  onReady:function(){
+    // 页面渲染完成
+  },
+  onShow:function(){
+    // 页面显示
+  },
+  onHide:function(){
+    // 页面隐藏
+  },
+  onUnload:function(){
+    // 页面关闭
+  }
+})
